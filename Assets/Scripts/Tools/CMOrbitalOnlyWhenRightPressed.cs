139ef6ba6c4d1604d74d122d2c111450db48c113
ChristopheClaustre <christophe.claustre.31@gmail.com>
2021-10-03 11:22:13 +0200

    [FCT] Use of cinemachine to have a orbital controller around the rocket in full display

---
+++ Assets/Scripts/Tools/CMOrbitalOnlyWhenRightPressed.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Cinemachine;
+
+public class CMOrbitalOnlyWhenRightPressed : MonoBehaviour
+{
+    void Start()
+    {
+        CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+
+    public float GetAxisCustom(string axisName)
+    {
+        if (axisName == "Mouse X")
+        {
+            if (Input.GetMouseButton(1))
+            {
+                return UnityEngine.Input.GetAxis("Mouse X");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return UnityEngine.Input.GetAxis(axisName);
+    }
+}
