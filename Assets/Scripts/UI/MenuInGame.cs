c68c6324c015bd085022fddb398c8688d3c7b548
Xavier Chalut <xavier.chalut@gmail.com>
2021-10-04 22:11:38 +0200

    [FCT] Add menu ingame to leave - change view - validate the rocket

---
+++ Assets/Scripts/UI/MenuInGame.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuInGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onQuit()
+    {
+        //Do something maybe...
+    }
+
+    public void onOverview()
+    {
+        //Do something maybe...
+    }
+
+    public void onValidate()
+    {
+        //Do something maybe...
+    }
+}
