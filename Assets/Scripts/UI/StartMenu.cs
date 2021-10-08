21c7957e167e7c21ced28272fb0399bc11d827e2
ChristopheClaustre <christophe.claustre.31@gmail.com>
2021-10-08 23:34:33 +0200

    [FCT] StartMenu: The default difficulty level is now at 1 (before it was random I guess)

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -13,7 +13,7 @@ public class StartMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        onLevel1Toggled(true);
     }
 
     // Update is called once per frame
