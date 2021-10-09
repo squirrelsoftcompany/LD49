c85d8de8f4e2b2ecceff9782bce3bd21fa1c4430
ChristopheClaustre <christophe.claustre.31@gmail.com>
2021-10-09 04:58:22 +0200

    [FCT] Update of all activable animations and launch them by code when the user interact with the rocket

--- Assets/Scripts/Activable/ActivableFreezer.cs
+++ Assets/Scripts/Activable/ActivableFreezer.cs
@@ -4,43 +4,90 @@ using UnityEngine;
 
 public class ActivableFreezer : ActivableBehaviour
 {
+    enum FreezerStatus
+    {
+        eReady = 0,
+        eOn,
+        eOff,
+        eStatusNb
+    }
 
-    //TODO : We need a timer to set active at false when module stop the freeze action
-    /*
-    private float mFreezeDuration = 5.0f;
-    private float mFreezeDurationTimeRemaining = 0.0f;*/
+    public float actionTime = 0.0f;
+    public float cooldownTime = 0.0f;
+
+    private float timeRemaining = 0.0f;
+    private FreezerStatus freezerStatus = FreezerStatus.eReady;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        switch (freezerStatus)
+        {
+            case FreezerStatus.eReady:
+                // Do nothing
+                break;
+            case FreezerStatus.eOn:
+                if (timeRemaining > 0)
+                {
+                    timeRemaining -= Time.deltaTime;
+                }
+                else
+                {
+                    freezerStatus = FreezerStatus.eOff;
+                    Stop();
+                    timeRemaining = cooldownTime;
+                }
+                break;
+            case FreezerStatus.eOff:
+                if (timeRemaining > 0)
+                {
+                    timeRemaining -= Time.deltaTime;
+                }
+                else
+                {
+                    freezerStatus = FreezerStatus.eReady;
+                    Ready();
+                }
+                break;
+        }
+    }
 
     public override void clickDownBehavior()
     {
-        if (!isActive)
+        switch (freezerStatus)
         {
-            Active();
+            case FreezerStatus.eReady:
+                freezerStatus = FreezerStatus.eOn;
+                Active();
+                timeRemaining = actionTime;
+                isActive = true;
+                break;
+            case FreezerStatus.eOn:
+                freezerStatus = FreezerStatus.eOff;
+                Stop();
+                isActive = false;
+                break;
+            case FreezerStatus.eOff:
+                // Do nothing
+                break;
         }
-        else
-        {
-            Stop();
-        }
-        isActive = !isActive;
     }
-
-    public override void clickUpBehavior()
-    {
-        //Do nothing.
-    }
-
-    public override void mouseExit()
-    {
-        //Do nothing.
-    }
-
 
     public override void Active()
     {
         mParentModule.GetComponent<ModuleBehavior>().activeFreeze(true);
+        GetComponentInChildren<Animator>()?.SetTrigger("On");
     }
 
     public override void Stop()
     {
-        mParentModule.GetComponent<ModuleBehavior>().activeFreeze(true);
+        mParentModule.GetComponent<ModuleBehavior>().activeFreeze(false);
+        GetComponentInChildren<Animator>()?.SetTrigger("Off");
+    }
+
+    public void Ready()
+    {
+        GetComponentInChildren<Animator>()?.SetTrigger("Ready");
     }
 }
--- Assets/Scripts/Activable/ActivablePressureGeneral.cs
+++ Assets/Scripts/Activable/ActivablePressureGeneral.cs
@@ -7,11 +7,13 @@ public class ActivablePressureGeneral : ActivablePressure
     public override void Active()
     {
         System.Array.ForEach(FindObjectsOfType<ModuleBehavior>(), x => x.activePressureEvacuation(true));
+        GetComponentInChildren<Animator>()?.SetBool("On", true);
     }
 
     public override void Stop()
     {
         System.Array.ForEach(FindObjectsOfType<ModuleBehavior>(), x => x.activePressureEvacuation(false));
+        GetComponentInChildren<Animator>()?.SetBool("On", false);
     }
 
     public override PovManager.RocketPOV GetRocketPOV()
